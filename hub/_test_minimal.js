475caa0c4735ede1c1a8934aa71b66cbb324fd93
wzk1 <wzkphy@163.com>
2026-06-21 07:31:27 +0800

    chore: remove debug test files

--- hub/_test_minimal.js
+++
@@ -1,1 +0,0 @@
-  var links='';if(d.links&&d.links.length)links='<div class="d-links">'+d.links.map(function(l){var host=l.replace(/https?:\/\//,'').split('/')[0];return'<a href="'+l+'" target="_blank">'+host+'</a>'}).join(' · ')+'</div>';
